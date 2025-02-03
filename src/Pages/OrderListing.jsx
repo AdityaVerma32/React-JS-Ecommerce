7ce96cd53a6b6fcea4ec10a4c4bb07bd32ee83b0
Aditya Verma <vermapradeeep5@gmail.com>
2025-02-03 23:30:51 +0530

    Made Order Listing and User Listing page

---
+++ src/Pages/OrderListing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OrderListing() {
+  return (
+    <div>
+    Order Listing
+    </div>
+  )
+}
+
+export default OrderListing
---
+++ src/Pages/UsersListing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UsersListing() {
+  return (
+    <div>
+      List of Users
+    </div>
+  )
+}
+
+export default UsersListing
